62ecdc5dd484aceaedc8ab4b71b680cfffaa5fbd
bagherhasani0 <bah3944@utulsa.edu>
2026-03-10 21:29:53 -0500

    Initial Commit

---
+++ PriceSnap/PriceSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceSnapApp.swift
+//  PriceSnap
+//
+//  Created by Baqer Hassani on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct PriceSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
